4bcae73f76f33f95b0d494ee8efe4660a9173d67
N�kolas Cardona <nikolasca@unisabana.edu.co>
2019-05-11 17:09:01 -0500

    123

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "Hello! This is a demo application linked to this tutorial: http://jkutner.github.io/2016/08/18/android-backend-api-heroku-retrofit.html";
+  }
+
+  @RequestMapping("/hello")
+  @ResponseBody
+  String hello() {
+    return "Hello from Heroku!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
