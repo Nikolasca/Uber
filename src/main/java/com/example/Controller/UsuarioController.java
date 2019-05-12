733057e663d19aab325257b790434b155a8c7bdc
N�kolas Cardona <nikolasca@unisabana.edu.co>
2019-05-11 22:26:17 -0500

    Add Controller User

--- src/main/java/com/example/Controller/UsuarioController.java
+++ src/main/java/com/example/Controller/UsuarioController.java
@@ -1,5 +1,15 @@
-public class UsuarioController{
+package com.example.Controller;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/servicio")
+public class UsuarioController{
+    @RequestMapping(method = RequestMethod.GET)
+    public String  Hola() {
+        return "Hola Níkolas";
+        
+    }
 
 
 
