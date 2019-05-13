d2af0d4021099fe6204b69f06034d882ae2856eb
N�kolas Cardona <nikolasca@unisabana.edu.co>
2019-05-12 20:48:28 -0500

    123

--- src/main/java/com/example/Controller/UsuarioController.java
+++ src/main/java/com/example/Controller/UsuarioController.java
@@ -18,7 +18,7 @@ public class UsuarioController{
 
     @RequestMapping(value = "/Hola", method = RequestMethod.POST)
     @ResponseBody
-    public String  Prueba(String name) {
+    public String  Test(String name) {
         return "Hola"+name ;
         
     }
