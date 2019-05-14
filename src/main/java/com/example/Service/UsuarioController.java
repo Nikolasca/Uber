58a244a8056f60d25e07d1004a6deccc77ee7736
nikolasca <nikolas_cardona@hotmail.com>
2019-05-14 01:50:15 -0500

    add json change

--- src/main/java/com/example/Service/UsuarioController.java
+++ src/main/java/com/example/Service/UsuarioController.java
@@ -77,7 +77,7 @@ public class UsuarioController{
         for (Usuario usuario : usuarios) {
             if ((usuario.getUsuario().compareTo(user) == 0) && (usuario.getPassword().compareTo(pass) == 0)) {
                 x = true;
-                return mensaje = "Usuario Aceptado";
+                return mensaje = "Usuario Aceptado,"+usuario.getUsuario()+","+usuario.getPassword();
             }
 
         }
