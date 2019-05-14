29e8c0b313dee577af192152cd159c5cc63f8765
nikolasca <nikolas_cardona@hotmail.com>
2019-05-13 22:45:14 -0500

    Add register

---
+++ src/main/java/com/example/Service/Singleton.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import AdapterPackage.Usuario;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikolasca
+ */
+public class Singleton {
+    private static ArrayList<Usuario> usuarios;
+    private static Singleton single;
+
+    public Singleton() {
+        usuarios = new ArrayList();
+    }
+    
+    
+    
+    public static Singleton getSingle() {
+        if (single == null) {
+            single = new Singleton();
+        }
+        return single;
+    }
+
+    public  ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public static void setUsuarios(ArrayList<Usuario> usuarios) {
+        Singleton.usuarios = usuarios;
+    }
+
+    public void Add(Usuario user) {
+        usuarios.add(user);
+    }
+
+    public Usuario getById(int id) {
+        Usuario user = null;
+
+        for (Usuario usuario : usuarios) {
+            if (id == usuario.getId()) {
+                user = usuario;
+
+            }
+
+        }
+        return user;
+    }
+
+
+}
+
--- src/main/java/com/example/Service/UsuarioController.java
+++ src/main/java/com/example/Service/UsuarioController.java
@@ -1,4 +1,11 @@
 package com.example.Controller;
+
+import java.util.ArrayList;
+import AdapterPackage.Adapter;
+import AdapterPackage.Administrador;
+import AdapterPackage.Conductor;
+import AdapterPackage.Pasajero;
+import AdapterPackage.Usuario;
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
@@ -22,5 +29,42 @@ public class UsuarioController{
         return "Hola"+name ;
         
     }
+    @RequestMapping(value = "/Register", method = RequestMethod.POST)
+    @ResponseBody
+    public String  Register(@RequestParam("name") String name, @RequestParam("pass") String pass, @RequestParam("type") String type) {
+        String mensaje = "";
+        if (C.compareTo("Conductor") == 0) {
+            Usuario user = new Conductor();
+            user.setId(s.getUsuarios().size() + 1);
+            user.setTipo_Usuario(C);
+            user.setUsuario(A);
+            user.setPassword(B);
+            s.Add(user);
+             mensaje = "Conductor Creado";
+            return  mensaje;
+
+        } else if (C.compareTo("Pasajero") == 0) {
+            Usuario user = new Pasajero();
+            user.setId(s.getUsuarios().size() + 1);
+            user.setTipo_Usuario(C);
+            user.setUsuario(A);
+            user.setPassword(B);
+            s.Add(user);
+             mensaje = "Pasajero Creado";
+            return  mensaje;
+        } else if (C.compareTo("Administrador") == 0) {
+            Usuario user = new Adapter();
+            user.setId(s.getUsuarios().size() + 1);
+            user.setTipo_Usuario(C);
+            user.setUsuario(A);
+            user.setPassword(B);
+            s.Add(user);
+            mensaje = "Administrador Creado";
+            return  mensaje;
+        }
+        return mensaje;
+   
+    }
+
 }
     
