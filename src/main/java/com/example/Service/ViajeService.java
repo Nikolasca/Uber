dbf3eeff7ee26f4847df297c1a0b30ace5199d04
nikolasca <nikolas_cardona@hotmail.com>
2019-05-18 00:44:11 -0500

    ViajeService

---
+++ src/main/java/com/example/Service/ViajeService.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import AdapterPackage.Adapter;
+import AdapterPackage.Administrador;
+import AdapterPackage.Conductor;
+import AdapterPackage.Pasajero;
+import AdapterPackage.Usuario;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+import servlet.Singleton;
+
+
+@Controller
+@RequestMapping("/servicio")
+private Singleton s = Singleton.getSingle();
+public class ViajeService{
+@RequestMapping(value = "/Nuevo_Viaje", method = RequestMethod.POST
+    @ResponseBody
+    public String  RegistroViaje(String punto_inicio, String punto_final, Pasajero Cliente, Conductor conductor) {
+        
+
+
+        return "hola" ;
+        
+    }
+}
